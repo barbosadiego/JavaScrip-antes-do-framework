4f97b46995e362857fbe45f5da5d4d0a9e835b24
barbosdiego <diego.gutemberg@hotmail.com>
2022-02-22 16:11:41 -0300

    js

---
+++ classes/script.js
@@ -0,0 +1,50 @@
+class SmoothScroll {
+  constructor(links) {
+    this.linkElements = document.querySelectorAll(links);
+    this.addClickEvent();
+  }
+  handleClick(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    window.scrollTo({
+      top: section.offsetTop - (window.innerHeight - section.clientHeight) / 2,
+      behavior: "smooth",
+    });
+  }
+  addClickEvent() {
+    this.linkElements.forEach((link) => {
+      link.addEventListener("click", this.handleClick);
+    });
+  }
+}
+
+class ActiveSmoothScroll extends SmoothScroll {
+  constructor(links, sections) {
+    super(links);
+    // extender com o super usando os argumentos da classe anterior
+    this.sectionElements = document.querySelectorAll(sections);
+    console.log(this.sectionElements);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.handleScroll()
+    this.activeScroll();
+  }
+  handleScroll() {
+    // console.log(this);
+    this.sectionElements.forEach((section, index) => {
+      if (window.pageYOffset > section.offsetTop - window.innerHeight * 0.5) {
+        this.linkElements[index].classList.add("active");
+      } else {
+        this.linkElements[index].classList.remove("active");
+      }
+    });
+  }
+  activeScroll() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+}
+
+const scroll = new ActiveSmoothScroll('a[href^="#"]', "section");
+
+// scroll.addClickEvent();
+// console.log(scroll.linkElements);
